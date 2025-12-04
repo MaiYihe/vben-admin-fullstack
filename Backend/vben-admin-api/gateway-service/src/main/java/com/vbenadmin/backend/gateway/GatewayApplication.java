966327fdb93124bf2be3b24d074c4c2d6e14fe06
maiyihe <2656377727@qq.com>
2025-12-05 01:17:20 +0800

    gateway 名称修正

--- Backend/vben-admin-api/gateway-service/src/main/java/com/vbenadmin/backend/gateway/GatewayApplication.java
+++ Backend/vben-admin-api/gateway-service/src/main/java/com/vbenadmin/backend/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.vbenadmin.backend.vbenadmingateway;
+package com.vbenadmin.backend.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
