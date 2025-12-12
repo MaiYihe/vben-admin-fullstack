584f1fb3198e34cdaed5c1d7f964117d5e4cb34e
maiyihe <2656377727@qq.com>
2025-12-13 01:38:58 +0800

    fix：dubbo 需要传输的类都加上 Serialize 接口并固定版本

--- Backend/vben-admin-api/common-rpc/src/main/java/com/vbenadmin/backend/commonrpc/models/dto/UserInfoDTO.java
+++ Backend/vben-admin-api/common-rpc/src/main/java/com/vbenadmin/backend/commonrpc/models/dto/UserInfoDTO.java
@@ -8,6 +8,8 @@ import java.io.Serializable;
 @Data
 @Builder
 public class UserInfoDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String id;
     private String username;
     private String password;
--- Backend/vben-admin-api/common-rpc/src/main/java/com/vbenadmin/backend/commonrpc/models/request/UserCreateRequest.java
+++ Backend/vben-admin-api/common-rpc/src/main/java/com/vbenadmin/backend/commonrpc/models/request/UserCreateRequest.java
@@ -1,11 +1,15 @@
 package com.vbenadmin.backend.commonrpc.models.request;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class UserCreateRequest {
+public class UserCreateRequest implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String username;
     private String password;
 }
