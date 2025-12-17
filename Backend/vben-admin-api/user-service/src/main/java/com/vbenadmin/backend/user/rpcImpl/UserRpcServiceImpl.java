12870cef48b880faf74ec46d85fdf4ff6ee3f27d
maiyihe <2656377727@qq.com>
2025-12-18 00:17:20 +0800

    fix：[user-service] 模块重构，不在 rpcImpl 里面写裁决，而是写编排
    rpcImpl 作为内部的"controller"，职责是编排而不是裁决

--- Backend/vben-admin-api/user-service/src/main/java/com/vbenadmin/backend/user/rpcImpl/UserRpcServiceImpl.java
+++ Backend/vben-admin-api/user-service/src/main/java/com/vbenadmin/backend/user/rpcImpl/UserRpcServiceImpl.java
@@ -1,57 +1,33 @@
 package com.vbenadmin.backend.user.rpcImpl;
 
-import com.vbenadmin.backend.commoncore.exception.BizException;
+import org.apache.dubbo.config.annotation.DubboService;
+
 import com.vbenadmin.backend.commonrpc.models.dto.UserInfoDTO;
 import com.vbenadmin.backend.commonrpc.models.request.UserCreateRequest;
 import com.vbenadmin.backend.commonrpc.rpc.IUserRpcService;
-import com.vbenadmin.backend.user.converter.IUserConverter;
 import com.vbenadmin.backend.user.entity.User;
 import com.vbenadmin.backend.user.service.IUserService;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.dubbo.config.annotation.DubboService;
 
 @DubboService
 @RequiredArgsConstructor
 public class UserRpcServiceImpl implements IUserRpcService {
     private final IUserService userService;
-    private final IUserConverter userConverter;
 
     @Override
     public UserInfoDTO getUserInfoByUserName(String username) {
-        return userService.lambdaQuery()
-                .eq(User::getUsername, username)
-                .oneOpt()
-                .map(userConverter::toUserInfoDTO)
-                .orElse(null);
-
+        return userService.getUserInfoByUserName(username);
     }
 
     @Override
     public boolean existUser(String username) {
-        User user = userService.lambdaQuery()
-                .eq(User::getUsername, username)
-                .oneOpt()
-                .orElse(null);
-
-        return user != null;
+        return userService.existUser(username);
     }
 
     @Override
     public String createUser(UserCreateRequest userCreateRequest) {
-        // 创建 User 实体
-        User user = new User();
-        user.setUsername(userCreateRequest.getUsername());
-        user.setPassword(userCreateRequest.getPassword());
-
-        // 保存用户
-        boolean success = userService.save(user);
-        // 保存失败处理
-        if (!success) {
-            throw new BizException(50001, "用户保存失败");
-        }
-
+        User user = userService.createUser(userCreateRequest);
         return user.getId();
     }
-
 }
--- Backend/vben-admin-api/user-service/src/main/java/com/vbenadmin/backend/user/service/IUserService.java
+++ Backend/vben-admin-api/user-service/src/main/java/com/vbenadmin/backend/user/service/IUserService.java
@@ -1,6 +1,8 @@
 package com.vbenadmin.backend.user.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.vbenadmin.backend.commonrpc.models.dto.UserInfoDTO;
+import com.vbenadmin.backend.commonrpc.models.request.UserCreateRequest;
 import com.vbenadmin.backend.user.entity.User;
 import com.vbenadmin.backend.user.models.vo.UserProfileVO;
 
@@ -13,5 +15,9 @@ import com.vbenadmin.backend.user.models.vo.UserProfileVO;
  * @since 2025-11-06
  */
 public interface IUserService extends IService<User> {
+    UserInfoDTO getUserInfoByUserName(String username);
+    boolean existUser(String username);
+    User createUser(UserCreateRequest userCreateRequest);
+
     UserProfileVO getUserProfile();
 }
